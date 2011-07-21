a532f7b30cb9acddf66b73ce4821afa324fbc867
Taliesin Sisson <taliesins@yahoo.com>
2011-07-21 20:02:14 +0100

    Stretch forms to fill up window

--- Talifun.Commander.Command.AntiVirus/Configuration/AntiVirusElementPanel.xaml.cs
+++ Talifun.Commander.Command.AntiVirus/Configuration/AntiVirusElementPanel.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Talifun.Commander.Command.Configuration;
+﻿using System.ComponentModel;
+using System.Configuration;
+using System.Windows;
+using Talifun.Commander.Command.Configuration;
 
 namespace Talifun.Commander.Command.AntiVirus.Configuration
 {
@@ -11,6 +14,38 @@ namespace Talifun.Commander.Command.AntiVirus.Configuration
         {
             Settings = AntiVirusConfiguration.Instance;
             InitializeComponent();
+
+			BindToElement += OnBindToElement;
         }
+
+		private AntiVirusElement Element { get; set; }
+
+		private void OnBindToElement(object sender, BindToElementEventArgs e)
+		{
+			if (Element != null)
+			{
+				Element.PropertyChanged -= OnElementPropertyChanged;
+			}
+
+			if (e.Element == null || !(e.Element is AntiVirusElement)) return;
+			Element = e.Element as AntiVirusElement;
+
+			SaveButton.IsEnabled = false;
+
+			this.DataContext = Element;
+
+			Element.PropertyChanged += OnElementPropertyChanged;
+		}
+
+		void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			SaveButton.IsEnabled = true;
+		}
+
+		private void SaveButtonClick(object sender, RoutedEventArgs e)
+		{
+			SaveButton.IsEnabled = false;
+			Element.CurrentConfiguration.Save(ConfigurationSaveMode.Minimal);
+		}
     }
 }
