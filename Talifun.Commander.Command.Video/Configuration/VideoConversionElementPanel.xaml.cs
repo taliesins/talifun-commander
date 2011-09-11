14cf12bba1e5a2c297498b2089bfc252eaf1b818
Taliesin Sisson <taliesins@yahoo.com>
2011-09-11 10:52:35 +0100

    Common video format drop down to set common settings.

--- Talifun.Commander.Command.Video/Configuration/VideoConversionElementPanel.xaml.cs
+++ Talifun.Commander.Command.Video/Configuration/VideoConversionElementPanel.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Configuration;
+using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using System.Xml;
 using Talifun.Commander.Command.Configuration;
 
 namespace Talifun.Commander.Command.Video.Configuration
@@ -47,5 +51,98 @@ namespace Talifun.Commander.Command.Video.Configuration
 			SaveButton.IsEnabled = false;
 			Element.CurrentConfiguration.Save(ConfigurationSaveMode.Minimal);
 		}
+
+		private void WidthTextBoxTextChanged(object sender, TextChangedEventArgs e)
+		{
+			SetSelectedVideoFormat();
+		}
+
+		private void HeightTextBoxTextChanged(object sender, TextChangedEventArgs e)
+		{
+			SetSelectedVideoFormat();
+		}
+
+		private void AspectRatioComboBoxSelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			SetSelectedVideoFormat();
+		}
+
+		private void SetSelectedVideoFormat()
+		{
+			if (_selectionBoxChanged) return;
+
+			var textWidth = widthTextBox.Text;
+			int width;
+			if (!int.TryParse(textWidth, out width))
+			{
+				width = 0;
+			}
+
+			var textHeight = heightTextBox.Text;
+			int height;
+			if (!int.TryParse(textHeight, out height))
+			{
+				height = 0;
+			}
+
+			var aspectRatio = AspectRatio.NotSpecified;
+			if (aspectRatioComboBox.SelectedItem is AspectRatio)
+			{
+				aspectRatio = (AspectRatio)aspectRatioComboBox.SelectedItem;
+			}
+			
+			var xmlElements = (ReadOnlyObservableCollection<XmlNode>)videoFormatComboBox.ItemsSource;
+			var aspectRatioConverter = new EnumConverter(typeof(AspectRatio));
+			var selectedXmlElement =
+				xmlElements.Where(x => 
+					int.Parse(x.Attributes["width"].Value) == width 
+					&& int.Parse(x.Attributes["height"].Value) == height
+					&& (AspectRatio)aspectRatioConverter.ConvertFromString(x.Attributes["aspectRatio"].Value) == aspectRatio)
+				.FirstOrDefault() 
+				?? xmlElements.Where(x => 
+					int.Parse(x.Attributes["width"].Value) == 0 
+					&& int.Parse(x.Attributes["height"].Value) == 0
+					&& (AspectRatio)aspectRatioConverter.ConvertFromString(x.Attributes["aspectRatio"].Value) == AspectRatio.NotSpecified)
+				.First();
+
+			if (videoFormatComboBox.SelectionBoxItem != selectedXmlElement)
+			{
+				videoFormatComboBox.SelectedItem = selectedXmlElement;
+			}
+		}
+
+
+    	private bool _selectionBoxChanged = false;
+    	private void VideoFormatComboBoxSelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+		{
+    		if (e.AddedItems.Count <= 0) return;
+
+    		_selectionBoxChanged = true;
+    		var selectedVideoFormat = e.AddedItems[0] as XmlNode;
+    		var width = int.Parse(selectedVideoFormat.Attributes["width"].Value);
+    		var height = int.Parse(selectedVideoFormat.Attributes["height"].Value);
+
+			var aspectRatioConverter = new EnumConverter(typeof(AspectRatio));
+
+    		var aspectRatio = (AspectRatio) aspectRatioConverter.ConvertFromString(selectedVideoFormat.Attributes["aspectRatio"].Value);
+
+    		if (width > 0 && Element.Width != width)
+    		{
+    			Element.Width = width;
+    		}
+
+    		if (height > 0 && Element.Height != height)
+    		{
+    			Element.Height = height;
+    		}
+
+			if (Element.AspectRatio != aspectRatio)
+			{
+				Element.AspectRatio = aspectRatio;
+			}
+    		_selectionBoxChanged = false;
+		}
+
+
     }
 }
