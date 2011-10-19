ea00453c1e28978d687e0a910ec2a61b78808001
Taliesin Sisson <taliesins@yahoo.com>
2011-10-19 14:41:33 +0100

    Add configuration for Video Thumbnailer collection

---
+++ Talifun.Commander.Command.VideoThumbNailer/Configuration/VideoThumbnailerElementCollectionPanel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using Talifun.Commander.Command.Configuration;
+using Talifun.Commander.UI;
+
+namespace Talifun.Commander.Command.VideoThumbNailer.Configuration
+{
+	/// <summary>
+	/// Interaction logic for VideoThumbnailerElementCollectionPanel.xaml
+	/// </summary>
+	public partial class VideoThumbnailerElementCollectionPanel : ElementCollectionPanelBase
+	{
+		public VideoThumbnailerElementCollectionPanel()
+		{
+			Settings = VideoThumbnailerConfiguration.Instance;
+			InitializeComponent();
+			BindToElementCollection += OnBindToElementCollection;
+		}
+
+		private VideoThumbnailerElementCollectionPanelDataModel DataModel { get; set; }
+
+		private void OnBindToElementCollection(object sender, BindToElementCollectionEventArgs e)
+		{
+			DataModel = new VideoThumbnailerElementCollectionPanelDataModel(e.AppSettings);
+			this.DataContext = DataModel;
+		}
+
+		private void fFMpegPathButton_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				FileName = fFMpegPathTextBox.Text,
+				Multiselect = false
+			};
+
+			var result = openFileDialog.ShowDialog(this.GetIWin32Window());
+			if (result != DialogResult.OK) return;
+
+			var foldername = openFileDialog.FileName;
+			fFMpegPathTextBox.Text = foldername;
+		}
+	}
+}
---
+++ Talifun.Commander.Command.VideoThumbNailer/Configuration/VideoThumbnailerElementCollectionPanelDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Talifun.Commander.Command.Configuration;
+
+namespace Talifun.Commander.Command.VideoThumbNailer.Configuration
+{
+	public class VideoThumbnailerElementCollectionPanelDataModel : DataModelBase
+	{
+		public VideoThumbnailerElementCollectionPanelDataModel(AppSettingsSection appSettings)
+		{
+			AppSettings = appSettings;
+		}
+
+		private AppSettingsSection AppSettings { get; set; }
+
+		public string FFMpegPath
+		{
+			get { return AppSettings.Settings[VideoThumbnailerConfiguration.Instance.FFMpegPathSettingName].Value; }
+			set
+			{
+				if (AppSettings.Settings[VideoThumbnailerConfiguration.Instance.FFMpegPathSettingName].Value == value) return;
+
+				OnPropertyChanging("FFMpegPath");
+				AppSettings.Settings[VideoThumbnailerConfiguration.Instance.FFMpegPathSettingName].Value = value;
+				OnPropertyChanged("FFMpegPath");
+			}
+		}
+	}
+}
