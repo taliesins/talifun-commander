20ce7970580af8e391551a0cfb792a664a60f887
Taliesin Sisson <taliesins@yahoo.com>
2011-10-19 14:25:50 +0100

    Add configuration for Image Conversion

---
+++ Talifun.Commander.Command.Image/Configuration/ImageConversionElementCollectionPanel.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using Talifun.Commander.Command.Configuration;
+using Talifun.Commander.UI;
+
+namespace Talifun.Commander.Command.Image.Configuration
+{
+	/// <summary>
+	/// Interaction logic for ImageConversionElementCollectionPanel.xaml
+	/// </summary>
+	public partial class ImageConversionElementCollectionPanel : ElementCollectionPanelBase
+	{
+		public ImageConversionElementCollectionPanel()
+		{
+			Settings = ImageConversionConfiguration.Instance;
+			InitializeComponent();
+			BindToElementCollection += OnBindToElementCollection;
+		}
+
+		private ImageConversionElementCollectionPanelDataModel DataModel { get; set; }
+
+		private void OnBindToElementCollection(object sender, BindToElementCollectionEventArgs e)
+		{
+			DataModel = new ImageConversionElementCollectionPanelDataModel(e.AppSettings);
+			this.DataContext = DataModel;
+		}
+
+		private void convertPathButton_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				FileName = convertPathTextBox.Text,
+				Multiselect = false
+			};
+
+			var result = openFileDialog.ShowDialog(this.GetIWin32Window());
+			if (result != DialogResult.OK) return;
+
+			var foldername = openFileDialog.FileName;
+			convertPathTextBox.Text = foldername;
+		}
+	}
+}
---
+++ Talifun.Commander.Command.Image/Configuration/ImageConversionElementCollectionPanelDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Talifun.Commander.Command.Configuration;
+
+namespace Talifun.Commander.Command.Image.Configuration
+{
+	public class ImageConversionElementCollectionPanelDataModel : DataModelBase
+	{
+		public ImageConversionElementCollectionPanelDataModel(AppSettingsSection appSettings)
+		{
+			AppSettings = appSettings;
+		}
+
+		private AppSettingsSection AppSettings { get; set; }
+
+		public string ConvertPath
+		{
+			get { return AppSettings.Settings[ImageConversionConfiguration.Instance.ConvertPathSettingName].Value; }
+			set
+			{
+				if (AppSettings.Settings[ImageConversionConfiguration.Instance.ConvertPathSettingName].Value == value) return;
+
+				OnPropertyChanging("ConvertPath");
+				AppSettings.Settings[ImageConversionConfiguration.Instance.ConvertPathSettingName].Value = value;
+				OnPropertyChanged("ConvertPath");
+			}
+		}
+	}
+}
