ab9967c26f17b8874518e59fca61f15a35971059
Taliesin Sisson <taliesins@yahoo.com>
2011-10-19 14:35:52 +0100

    Add configuration for Video Conversion Collection

---
+++ Talifun.Commander.Command.Video/Configuration/VideoConversionElementCollectionPanel.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+using System.Windows.Forms;
+using Talifun.Commander.Command.Configuration;
+using Talifun.Commander.UI;
+
+namespace Talifun.Commander.Command.Video.Configuration
+{
+	/// <summary>
+	/// Interaction logic for VideoConversionElementCollectionPanel.xaml
+	/// </summary>
+	public partial class VideoConversionElementCollectionPanel : ElementCollectionPanelBase
+	{
+		public VideoConversionElementCollectionPanel()
+		{
+			Settings = VideoConversionConfiguration.Instance;
+			InitializeComponent();
+			BindToElementCollection += OnBindToElementCollection;
+		}
+
+		private VideoConversionElementCollectionPanelDataModel DataModel { get; set; }
+
+		private void OnBindToElementCollection(object sender, BindToElementCollectionEventArgs e)
+		{
+			DataModel = new VideoConversionElementCollectionPanelDataModel(e.AppSettings);
+			this.DataContext = DataModel;
+		}
+
+		private void fFMpegPathButton_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				FileName = fFMpegPathTextBox.Text,
+				Multiselect = false
+			};
+
+			var result = openFileDialog.ShowDialog(this.GetIWin32Window());
+			if (result != DialogResult.OK) return;
+
+			var foldername = openFileDialog.FileName;
+			fFMpegPathTextBox.Text = foldername;
+		}
+
+		private void flvTool2PathButton_Click(object sender, RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				FileName = flvTool2PathTextBox.Text,
+				Multiselect = false
+			};
+
+			var result = openFileDialog.ShowDialog(this.GetIWin32Window());
+			if (result != DialogResult.OK) return;
+
+			var foldername = openFileDialog.FileName;
+			flvTool2PathTextBox.Text = foldername;
+		}
+	}
+}
---
+++ Talifun.Commander.Command.Video/Configuration/VideoConversionElementCollectionPanelDataModel.cs
@@ -0,0 +1,41 @@
+﻿using System.Configuration;
+using Talifun.Commander.Command.Configuration;
+
+namespace Talifun.Commander.Command.Video.Configuration
+{
+	public class VideoConversionElementCollectionPanelDataModel : DataModelBase
+	{
+		public VideoConversionElementCollectionPanelDataModel(AppSettingsSection appSettings)
+		{
+			AppSettings = appSettings;
+		}
+
+		private AppSettingsSection AppSettings { get; set; }
+
+		public string FFMpegPath
+		{
+			get { return AppSettings.Settings[VideoConversionConfiguration.Instance.FFMpegPathSettingName].Value; }
+			set
+			{
+				if (AppSettings.Settings[VideoConversionConfiguration.Instance.FFMpegPathSettingName].Value == value) return;
+
+				OnPropertyChanging("FFMpegPath");
+				AppSettings.Settings[VideoConversionConfiguration.Instance.FFMpegPathSettingName].Value = value;
+				OnPropertyChanged("FFMpegPath");
+			}
+		}
+
+		public string FlvTool2Path
+		{
+			get { return AppSettings.Settings[VideoConversionConfiguration.Instance.FlvTool2PathSettingName].Value; }
+			set
+			{
+				if (AppSettings.Settings[VideoConversionConfiguration.Instance.FlvTool2PathSettingName].Value == value) return;
+
+				OnPropertyChanging("FlvTool2Path");
+				AppSettings.Settings[VideoConversionConfiguration.Instance.FlvTool2PathSettingName].Value = value;
+				OnPropertyChanged("FlvTool2Path");
+			}
+		}
+	}
+}
