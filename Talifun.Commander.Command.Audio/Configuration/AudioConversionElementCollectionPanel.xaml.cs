f3f83f0194f9233997ef368f7dacd65c6f12ec1a
Taliesin Sisson <taliesins@yahoo.com>
2011-10-19 14:05:09 +0100

    Add configuration for global audio conversion settings

---
+++ Talifun.Commander.Command.Audio/Configuration/AudioConversionElementCollectionPanel.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Forms;
+using Talifun.Commander.Command.Configuration;
+using Talifun.Commander.UI;
+
+namespace Talifun.Commander.Command.Audio.Configuration
+{
+	/// <summary>
+	/// Interaction logic for AudioConversionElementCollectionPanel.xaml
+	/// </summary>
+	public partial class AudioConversionElementCollectionPanel : ElementCollectionPanelBase
+	{
+		public AudioConversionElementCollectionPanel()
+		{
+			Settings = AudioConversionConfiguration.Instance;
+			InitializeComponent();
+			BindToElementCollection += OnBindToElementCollection;
+		}
+
+		private AudioConversionElementCollectionPanelDataModel DataModel { get; set; }
+
+		private void OnBindToElementCollection(object sender, BindToElementCollectionEventArgs e)
+		{
+			DataModel = new AudioConversionElementCollectionPanelDataModel(e.AppSettings);
+			this.DataContext = DataModel;
+		}
+
+		private void fFMpegPathButton_Click(object sender, System.Windows.RoutedEventArgs e)
+		{
+			var openFileDialog = new OpenFileDialog
+			{
+				FileName = fFMpegPathTextBox.Text,
+				Multiselect = false
+			};
+
+			var result = openFileDialog.ShowDialog(this.GetIWin32Window());
+			if (result != DialogResult.OK) return;
+
+			var foldername = openFileDialog.FileName;
+			fFMpegPathTextBox.Text = foldername;
+		}
+	}
+}
---
+++ Talifun.Commander.Command.Audio/Configuration/AudioConversionElementCollectionPanelDataModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Configuration;
+using Talifun.Commander.Command.Configuration;
+
+namespace Talifun.Commander.Command.Audio.Configuration
+{
+	public class AudioConversionElementCollectionPanelDataModel : DataModelBase
+	{
+		public AudioConversionElementCollectionPanelDataModel(AppSettingsSection appSettings)
+		{
+			AppSettings = appSettings;
+		}
+
+		private AppSettingsSection AppSettings { get; set; }
+
+		public string FFMpegPath
+		{
+			get { return AppSettings.Settings[AudioConversionConfiguration.Instance.FFMpegPathSettingName].Value; }
+			set
+			{
+				if (AppSettings.Settings[AudioConversionConfiguration.Instance.FFMpegPathSettingName].Value == value) return;
+
+				OnPropertyChanging("FFMpegPath");
+				AppSettings.Settings[AudioConversionConfiguration.Instance.FFMpegPathSettingName].Value = value;
+				OnPropertyChanged("FFMpegPath");
+			}
+		}
+	}
+}
