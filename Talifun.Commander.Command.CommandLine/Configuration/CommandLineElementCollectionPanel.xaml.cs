52d5f8401e9fc630eae7b364f0bf0f9b8e003bd3
Taliesin Sisson <taliesins@yahoo.com>
2011-10-19 14:16:18 +0100

    Add configuration for command line collection

---
+++ Talifun.Commander.Command.CommandLine/Configuration/CommandLineElementCollectionPanel.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Talifun.Commander.Command.Configuration;
+
+namespace Talifun.Commander.Command.CommandLine.Configuration
+{
+	/// <summary>
+	/// Interaction logic for CommandLineElementCollectionPanel.xaml
+	/// </summary>
+	public partial class CommandLineElementCollectionPanel : ElementCollectionPanelBase
+	{
+		public CommandLineElementCollectionPanel()
+		{
+			Settings = CommandLineConfiguration.Instance;
+			InitializeComponent();
+			BindToElementCollection += OnBindToElementCollection;
+		}
+
+		private CommandLineElementCollectionPanelDataModel DataModel { get; set; }
+
+		private void OnBindToElementCollection(object sender, BindToElementCollectionEventArgs e)
+		{
+			DataModel = new CommandLineElementCollectionPanelDataModel(e.AppSettings);
+			this.DataContext = DataModel;
+		}
+	}
+}
---
+++ Talifun.Commander.Command.CommandLine/Configuration/CommandLineElementCollectionPanelDataModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using Talifun.Commander.Command.Configuration;
+
+namespace Talifun.Commander.Command.CommandLine.Configuration
+{
+	public class CommandLineElementCollectionPanelDataModel : DataModelBase
+	{
+		public CommandLineElementCollectionPanelDataModel(AppSettingsSection appSettings)
+		{
+			AppSettings = appSettings;
+		}
+
+		private AppSettingsSection AppSettings { get; set; }
+	}
+}
