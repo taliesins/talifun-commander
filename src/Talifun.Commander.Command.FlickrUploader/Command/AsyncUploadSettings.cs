fa5cc4648ad61d063e7341309610caf093506284
Taliesin Sisson <taliesins@yahoo.com>
2013-09-12 07:47:48 +0100

    Move project files into src directory Add build scripts

---
+++ src/Talifun.Commander.Command.FlickrUploader/Command/AsyncUploadSettings.cs
@@ -0,0 +1,11 @@
+﻿using System.IO;
+using Talifun.Commander.Command.FlickrUploader.Command.Request;
+
+namespace Talifun.Commander.Command.FlickrUploader.Command
+{
+	public class AsyncUploadSettings
+	{
+		public Stream InputStream { get; set; }
+		public IExecuteFlickrUploaderWorkflowMessage Message { get; set; }
+	}
+}
